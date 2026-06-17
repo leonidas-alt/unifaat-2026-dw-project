1f4339e7c29ff7c35b35b1700a9d6ab108c203fb
emysant <emysant04@gmail.com>
2026-06-17 20:29:36 -0300

    TF Aula 13 - Commands, Migrations e Seeds

--- app/Commands/ListTablesCommand.js
+++ app/Commands/ListTablesCommand.js
@@ -1,12 +1,26 @@
 import fs from 'node:fs/promises'
 import path from 'node:path'
 import { pathToFileURL } from 'node:url'
+import postgres from '../../database/connections/postgres.js'
 
 export default {
     name: 'liste-tables',
     description: 'Lista as tabelas do banco de dados',
 
     async handle() {
-        /** TF 13 */
+        const result = await postgres.query(`
+            SELECT table_name
+            FROM information_schema.tables
+            WHERE table_schema = 'public'
+            ORDER BY table_name
+        `)
+
+        console.log('Tabelas encontradas:')
+
+        result.rows.forEach(table => {
+            console.log(table.table_name)
+        })
+
+        await postgres.close()
     }
 }
